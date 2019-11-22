7a0f2734b1b2f427518b69fe9d225e90fc993efb
czach81 <christopherpzach@gmail.com>
2019-11-22 11:35:51 -0600

    Second Commit

---
+++ character.looping.py
@@ -0,0 +1,12 @@
+alphabet = 'abcdef'
+
+for letter in alphabet:
+  print(letter)
+
+"""
+you have a string here 
+a for in loop allows you to access them just like a string/collection
+'a', 'b', 'c', 'd' ect
+
+
+"""
---
+++ order_of_operations.py
@@ -0,0 +1,5 @@
+calculation = 8 + 2 * 5 - (9 + 2) ** 2
+
+print(calculation)
+
+#https://www.quora.com/Whats-the-difference-between-PEMDAS-and-PEDMAS
